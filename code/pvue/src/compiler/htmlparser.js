1fe87304727f617283382dc0b562d41da1b3217c
Dec-F <dec_fan@icloud.com>
2018-03-13 16:45:29 +0800

    html解析

--- code/pvue/src/compiler/htmlparser.js
+++ code/pvue/src/compiler/htmlparser.js
@@ -1,15 +1,55 @@
 var htmlparser = require('htmlparser2')
-
+let root, parentQueue = []
 var parser = new htmlparser.Parser({
-	onopentag: function (name, attribs) {
-		console.log('start-->', name, attribs);
-	},
-	ontext: function (text) {
-		console.log("text-->", text);
-	},
-	onclosetag: function (tagname) {
-		console.log('end-->', tagname);
-	}
+    onopentag: function (name, attribs) {
+        if (!root) {
+            root = createASTEle(name, attribs)
+            parentQueue.push(root)
+            return
+        }
+        let parent = parentQueue[parentQueue.length - 1]
+        let ASTEle = createASTEle(name, attribs)
+        if (!parent.children) {
+            parent.children = []
+        }
+        parent.children.push(ASTEle)
+        parentQueue.push(ASTEle)
+    },
+    ontext: function (text) {
+        let parent = parentQueue[parentQueue.length - 1]
+        let ASTText = createASTText(text)
+        if (!parent.children) {
+            parent.children = []
+        }
+        parent.children.push(ASTText)
+    },
+    onclosetag: function (tagname) {
+        parentQueue.pop()
+    }
 }, { decodeEntities: true });
-parser.write("Xyz <script type='text/javascript'>var foo = '<<bar>>';</ script>");
-parser.end();
+parser.write(`<div>ddd<input type="text" v-model="msg" required max="8" v-validate:field1.group1.group2>ddddd</div>`);
+
+
+console.log(JSON.stringify(root, null, 4));
+
+
+function createASTEle(name, attribs, parent) {
+
+    return {
+        tag: name,
+        type: 1,
+        attr: attribs
+    }
+}
+
+function createASTText(text) {
+    return {
+        tag: 'text',
+        type: 2,
+        text
+    }
+}
+
+
+
+
